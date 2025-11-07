841fe4d147d1f992a8034566f027bad77041b0bd
Divijeshvarma.a <divijeshvarma.a@pidatacenters.com>
2025-11-07 15:39:38 +0530

    multistage docker implement 3

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -24,7 +24,7 @@ retriever = (
 
 if __name__ == "__main__":
 
-    st.header("Pi Chatbot")
+    st.header("Pi Chatbot v1")
 
     # [DONT CHANGE] check if the messages list is empty. If empty, initialize the messages list
     if "messages" not in st.session_state:
